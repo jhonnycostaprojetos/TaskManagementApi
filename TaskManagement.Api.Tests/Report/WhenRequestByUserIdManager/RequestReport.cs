678ccd6af377f2e398c5db3190f305bb6c5058f2
Jhonny Costa <jhonnycosta@ibm.com>
2024-03-06 19:21:17 -0300

    Create Test For Report

---
+++ TaskManagement.Api.Tests/Report/WhenRequestByUserIdManager/RequestReport.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using TaskManagement.Api.Controllers;
+using TaskManagement.Application.DTOs.User;
+using TaskManagement.Application.Interfaces;
+
+namespace TaskManagement.Api.Tests.Report.WhenRequestByUserIdManager
+{
+    public class RequestReport
+    {
+        private ReportController _reportController;
+
+        [Fact(DisplayName = "ReportByUserIdManager")]
+
+        public async Task Report_By_UserIdManager()
+        {
+            var serviceReportMock = new Mock<IReportService>();
+
+            serviceReportMock.Setup(m => m.Get(It.IsAny<int>())).Returns(Task.FromResult((object)null));
+
+            _reportController = new ReportController(serviceReportMock.Object);
+
+            var result = await _reportController.GenerateReport(1);
+            Assert.True(result is OkObjectResult);
+        }
+    }
+}
